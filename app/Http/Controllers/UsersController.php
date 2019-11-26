7c18acd2157875dc770bd17a9e68eeedeeb7c172
Kylin <kylin87@qq.com>
2019-11-26 09:59:32 +0000

    编辑个人资料

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -4,10 +4,24 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\User;
+use App\Http\Requests\UserRequest;
 
 class UsersController extends Controller
 {
+    //个人中心
     public function show(User $user){
         return view('users.show',compact('user'));
     }
+
+    //编辑个人资料页面
+    public function edit(User $user){
+        return view('users.edit',compact('user'));
+    }
+
+    //保存提交的编辑个人信息
+    public function update(UserRequest $request, User $user){
+        $user->update($request->all());
+        return redirect()->route('users.show',$user->id)->with('success','个人资料编辑成功！');
+    }
+
 }
