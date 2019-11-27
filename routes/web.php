027daf70282567d6e53404440d9ae3e0c5c11241
Kylin <kylin87@qq.com>
2019-11-27 05:20:10 +0000

    生成话题骨架

--- routes/web.php
+++ routes/web.php
@@ -35,3 +35,5 @@ Route::post('email/resend', 'Auth\VerificationController@resend')->name('verific
 
 //用户的展示资料、编辑资料和保存资料
 Route::resource('users','UsersController',['only' => ['show','update','edit']]);
+
+Route::resource('topics', 'TopicsController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);
