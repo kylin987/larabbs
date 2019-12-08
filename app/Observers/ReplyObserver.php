5e76a4632134898beb98cd38a8d3194aeebd11e9
Kylin <kylin87@qq.com>
2019-12-08 12:21:38 +0000

    消息通知有新回复

--- app/Observers/ReplyObserver.php
+++ app/Observers/ReplyObserver.php
@@ -3,7 +3,8 @@
 namespace App\Observers;
 
 use App\Models\Reply;
-use App\Jobs\RepliesCount;
+//use App\Jobs\RepliesCount;
+use App\Notifications\TopicReplied;
 
 // creating, created, updating, updated, saving,
 // saved,  deleting, deleted, restoring, restored
@@ -12,10 +13,15 @@ class ReplyObserver
 {
     public function creating(Reply $reply)
     {
-        //$reply->topic->reply_count = $reply->topic->replies->count();
-        //$reply->topic->save();
-        //dispatch(new RepliesCount($reply));
         $reply->content = clean($reply->content, 'user_topic_body');
+    }
+
+    public function created(Reply $reply){
+        $reply->topic->reply_count = $reply->topic->replies->count();
+        $reply->topic->save();
+
+        // 通知话题作者有新的评论
+        $reply->topic->user->notify(new TopicReplied($reply));
     }
 
     public function updating(Reply $reply)
@@ -27,6 +33,6 @@ class ReplyObserver
     {
         //$reply->topic->reply_count = $reply->topic->replies->count();
         //$reply->topic->save();
-        dispatch(new RepliesCount($reply));
+        //dispatch(new RepliesCount($reply));
     }
 }
