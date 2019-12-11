713590578c9794eeb6cc9ef2338ddeb235660ae8
Kylin <kylin87@qq.com>
2019-12-11 05:51:44 +0000

    侧边栏推荐链接

--- app/Http/Controllers/CategoriesController.php
+++ app/Http/Controllers/CategoriesController.php
@@ -6,11 +6,12 @@ use Illuminate\Http\Request;
 use App\Models\Topic;
 use App\Models\Category;
 use App\Models\User;
+use App\Models\Link;
 
 class CategoriesController extends Controller
 {
     //分类显示帖子
-    public function show(Category $category, Request $request, Topic $topic, User $user){
+    public function show(Category $category, Request $request, Topic $topic, User $user, Link $link){
         //读取分类ID关联的话题，并按每20条分页
         $topics = $topic->WithOrder($request->order)
                         ->where('category_id', $category->id)
@@ -19,7 +20,11 @@ class CategoriesController extends Controller
 
         // 活跃用户列表
         $active_users = $user->getActiveUsers();
+
+        $links = $link->getAllCached();
+
+
         //传参变量话题和分类到模板中
-        return view('topics.index',compact('topics','category', 'active_users'));
+        return view('topics.index',compact('topics','category', 'active_users','links'));
     }
 }
