fc032653ac6aaf351f7cf23c64a230398c25b128
Kylin <kylin87@qq.com>
2019-12-11 04:56:11 +0000

    活跃用户

--- app/Http/Controllers/CategoriesController.php
+++ app/Http/Controllers/CategoriesController.php
@@ -5,17 +5,21 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Topic;
 use App\Models\Category;
+use App\Models\User;
 
 class CategoriesController extends Controller
 {
     //分类显示帖子
-    public function show(Category $category, Request $request, Topic $topic){
+    public function show(Category $category, Request $request, Topic $topic, User $user){
         //读取分类ID关联的话题，并按每20条分页
         $topics = $topic->WithOrder($request->order)
                         ->where('category_id', $category->id)
                         ->with('user','category')   // 预加载防止 N+1 问题
                         ->paginate(20);
+
+        // 活跃用户列表
+        $active_users = $user->getActiveUsers();
         //传参变量话题和分类到模板中
-        return view('topics.index',compact('topics','category'));
+        return view('topics.index',compact('topics','category', 'active_users'));
     }
 }
